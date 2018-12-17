e5cd0f07d9da8b31d0687eee43f42f480dbbad91
farrellkj2 <farrellkj2@gmail.com>
2018-12-17 14:56:26 -0500

    add anonymized qualtrics data from edit-a-thon

---
+++ editathon_analysis.R
@@ -0,0 +1,7 @@
+#### Script to pull in and make sense of EcoDAS Wiki edit-a-thon survey data ####
+
+# install.packages('pacman')
+pacman::p_load(tidyverse)
+
+raw <- read_csv('./data/WikiProject_hackathon_qualtricsData.csv') # Survey responses
+key <- read_csv('./data/questionKey.csv') # Question key
