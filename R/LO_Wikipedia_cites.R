ca745f919360ef78d8e5cc8d767d58415e4cbd9b
jzwart <jzwart@usgs.gov>
2020-04-27 15:01:38 -0400

    adding figures

--- R/LO_Wikipedia_cites.R
+++ R/LO_Wikipedia_cites.R
@@ -6,6 +6,7 @@ library(bibtex)
 library(RefManageR)
 library(tidyverse)
 library(ggplot2)
+library(wesanderson)
 
 # downloading Web of Science DOI's for L&O pubs and checking for references on Wikipedia through Altmetric results 
 # https://apps.webofknowledge.com/summary.do?product=WOS&search_mode=GeneralSearch&qid=32&SID=7EIZCGZqv946vAWRQCe
@@ -64,7 +65,38 @@ lo_wiki_year = lo_wiki_year %>% complete(journal, nesting(year)) # filling in mi
 lo_wiki_year$articles = ifelse(is.na(lo_wiki_year$articles), 0, lo_wiki_year$articles)
 lo_wiki_year$wikipedia_cites = ifelse(is.na(lo_wiki_year$wikipedia_cites), 0, lo_wiki_year$wikipedia_cites)
 
-fig_out = ggplot(lo_wiki_year, 
+## summarize number of pubs per year for each journal 
+lo_pubs_summary = tibble()
+for(i in 1:length(lo_pubs)){
+  cur_article = unlist(lo_pubs[[i]][1])
+  cur_out = tibble(author = as.character(cur_article$author[[1]]),
+                   title = cur_article$title, 
+                   year = strftime(strptime(cur_article$year,
+                                            format = '{%Y}'), format = '%Y'), 
+                   journal = cur_article$journal)
+  
+  lo_pubs_summary = bind_rows(lo_pubs_summary, cur_out)
+}
+
+
+lo_pubs_year = lo_pubs_summary %>% 
+  group_by(year, journal) %>% 
+  summarise(total_articles = n()) %>% 
+  ungroup()
+
+
+lo_pubs_year = lo_pubs_year %>% complete(journal, nesting(year)) # filling in missing years 
+lo_pubs_year$total_articles = ifelse(is.na(lo_pubs_year$total_articles), 0, lo_pubs_year$total_articles)
+
+plot_data = left_join(lo_pubs_year, lo_wiki_year, by = c('year', 'journal')) %>% 
+  mutate(articles =  ifelse(is.na(articles), 0, articles),
+         wikipedia_cites = ifelse(is.na(wikipedia_cites), 0, wikipedia_cites), 
+         percent_articles_on_wiki = articles / total_articles * 100,
+         total_articles = ifelse(is.na(percent_articles_on_wiki),NA, total_articles),
+         articles = ifelse(is.na(percent_articles_on_wiki),NA, articles))
+
+# plot 
+wiki_articles_plot = ggplot(plot_data, 
                  aes(x= as.Date(year, '%Y'), 
                      y = as.integer(articles),
                      fill = journal)) + 
@@ -74,5 +106,75 @@ fig_out = ggplot(lo_wiki_year,
   theme_classic() +
   theme(axis.title.x = element_blank()) 
 
-fig_out
+cols = wesanderson::wes_palette(name = 'Zissou1', n = 5)
+cols = cols[c(1,3,5)]
 
+percent_plot = ggplot(plot_data, 
+                      aes(x= as.numeric(year), 
+                          y = percent_articles_on_wiki,
+                          color = journal)) + 
+  geom_line(alpha = .7)+
+  geom_point(size = 3) +
+  theme_classic() +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_text(size = 16),
+        axis.text = element_text(size = 16),
+        legend.position = c(.2,.9), 
+        legend.title = element_blank()) + 
+  scale_color_manual(values = rev(cols), 
+                     labels = c('L&O: Methods', 'L&O: Letters', 'L&O')) + 
+  ylab('Articles cited on Wikipedia (%)')
+
+percent_plot
+
+# ggsave(filename = 'figures/LO_articles_wiki_perc.png', plot = percent_plot, width = 6,
+#        height = 4)
+
+total_articles = ggplot(plot_data, 
+                      aes(x= as.numeric(year), 
+                          y = total_articles,
+                          color = journal)) + 
+  geom_line(alpha = .7, show.legend = F)+
+  geom_point(size = 3, show.legend = F) +
+  theme_classic() +
+  theme(axis.title.x = element_blank(), axis.text.x = element_blank(), 
+        axis.title.y = element_text(size = 16),
+        axis.text = element_text(size = 16),
+        legend.position = c(.2,.9),
+        legend.title = element_blank()) + 
+  scale_color_manual(values = rev(cols), 
+                     labels = c('L&O: Methods', 'L&O: Letters', 'L&O')) + 
+  ylab('Articles Published (n)')
+
+total_articles
+
+# ggsave(filename = 'figures/LO_total_articles.png', plot = total_articles, width = 6,
+#        height = 4)
+
+articles_wiki = ggplot(plot_data, 
+                        aes(x= as.numeric(year), 
+                            y = as.integer(articles),
+                            color = journal)) + 
+  geom_line(alpha = .7, show.legend = F)+
+  geom_point(size = 3, show.legend = F) +
+  theme_classic() +
+  theme(axis.title.x = element_blank(), axis.text.x = element_blank(), 
+        axis.title.y = element_text(size = 16),
+        axis.text = element_text(size = 16),
+        legend.position = c(.2,.9), 
+        legend.title = element_blank()) + 
+  scale_color_manual(values = rev(cols), 
+                     labels = c('L&O: Methods', 'L&O: Letters', 'L&O')) + 
+  scale_y_continuous(breaks = scales::pretty_breaks()) + 
+  ylab('Articles cited on Wikipedia (n)')
+
+articles_wiki
+
+# ggsave(filename = 'figures/LO_articles_wiki.png', plot = articles_wiki, width = 6,
+#        height = 4)
+
+out = cowplot::plot_grid(total_articles, articles_wiki, percent_plot, 
+                         align = 'hv', nrow = 3, labels = c('a','b','c'))
+  
+ggsave(filename = 'figures/LO_articles_wiki_all.png', plot = out, width = 6,
+       height = 12)
